27a77f48727eb2ebd1777e16b4376796f53068c9
Remulo Costa <remulo.costa@gmail.com>
2020-07-20 12:16:30 -0300

    fix svg import

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,5 +1,24 @@
-declare module '*.png';
-
-declare module '*.JPG';
-
 declare module '*.gif';
+
+declare module '*.svg' {
+  import React = require('react');
+
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.json' {
+  const content: string;
+  export default content;
+}
