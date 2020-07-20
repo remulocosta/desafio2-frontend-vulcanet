af10327c2fbc51061e9345e2122f810247515875
Remulo Costa <remulo.costa@gmail.com>
2020-07-20 13:49:08 -0300

    test fix route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
-import SignIn from '../pages/SignIn';
+// import SignIn from '../pages/SignIn';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route exact path="/" component={SignIn} />
-    <Route exact path="/dashboard" component={Dashboard} isPrivate />
+    <Route exact path="/dashboard" component={Dashboard} />
+    {/* <Route exact path="/" component={SignIn} /> */}
   </Switch>
 );
 
